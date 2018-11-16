e8586fcc53f425cfa70e8bf03770e83c2ea343de
Mantas Gelezinis <Mantsvas@gmail.com>
2018-11-16 03:28:53 +0200

    Students List

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Asset\Packages;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Packages $assetsManager)
     {
+        $path = $this->get('kernel')->getProjectDir();
+        $file = file_get_contents($path . '/public/students.json');
+        $projects = json_decode($file, true);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'projects' => $projects,
         ]);
     }
 }
