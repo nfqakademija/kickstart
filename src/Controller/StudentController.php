a77ee994db979d376c1308d66ba490a6be5acb62
Mindaugas <mindaugas.bernotas@mediapark.com>
2018-11-17 12:57:32 +0200

    Added show student rezult

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,17 +3,35 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentController extends AbstractController
 {
     /**
-     * @Route("/student", name="student")
+     * @Route("/")
      */
     public function index()
     {
         return $this->render('student/index.html.twig', [
             'controller_name' => 'StudentController',
+        ]);
+    }
+
+    /**
+     * @Route("/student")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show(Request $request)
+    {
+        $name = $request->query->get('name');
+        $project = $request->query->get('project');
+
+
+        return $this->render('student/show.html.twig', [
+            'name' => $name,
+            'project' => $project
         ]);
     }
 }
