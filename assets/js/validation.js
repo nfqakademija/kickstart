1bc525dad5a6407b3039ca795e19466e68ef4916
Mindaugas <mindaugas.bernotas@mediapark.com>
2018-11-25 16:19:42 +0200

    Added validation

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -2,6 +2,7 @@ let projectName = document.getElementById('projectName');
 let validationResult = document.getElementById('validation-result');
 const validateName = function () {
     validationResult.innerText = 'Project name: ' + projectName.value;
+    validationResult.innerText = 'Validation with: ' + validationResult.dataset.path;
 };
 projectName.onkeyup = validateName;
 projectName.onchange = validateName;
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectController extends AbstractController
@@ -16,4 +19,23 @@ class ProjectController extends AbstractController
             'controller_name' => 'ProjectController',
         ]);
     }
+
+    /**
+     * @Route("/validate/{element}", name="validatePerson")
+     * @Method({"POST"})
+     * @param string $element
+     *
+     * @return JsonResponse
+     */
+    public function validate(string $element)
+    {
+        switch ($element) {
+            case 'projectName':
+                return new JsonResponse(['valid' => true]);
+        }
+        return new JsonResponse(['error' => 'Invalid method'], Response::HTTP_BAD_REQUEST);
+    }
 }
+
+
+
