b9b68238ba349cbdc3cb6097925190b909659234
Mindaugas <mindaugas.bernotas@mediapark.com>
2018-11-25 16:26:04 +0200

    Connected Frontend with Backend

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,8 +1,21 @@
+const axios = require('axios');
 let projectName = document.getElementById('projectName');
 let validationResult = document.getElementById('validation-result');
 const validateName = function () {
-    validationResult.innerText = 'Project name: ' + projectName.value;
-    validationResult.innerText = 'Validation with: ' + validationResult.dataset.path;
+    // validationResult.innerText = 'Project name: ' + projectName.value;
+    // validationResult.innerText = 'Validation with: ' + validationResult.dataset.path;
+    validationResult.innerText = '...';
+    axios.post(validationResult.dataset.path, {input: name.value})
+        .then(function(response) {
+            if (response.data.valid) {
+                validationResult.innerHTML = ":)";
+            } else {
+                validationResult.innerHTML = ":(";
+            }
+        })
+        .catch(function (error) {
+            validationResult.innerText = 'Error: ' + error;
+        });
 };
 projectName.onkeyup = validateName;
 projectName.onchange = validateName;
