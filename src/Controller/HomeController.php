30203115c55c64aa4191e72640370f5ec9a1c491
“Marijus84” <marijus.romanovas@hotmail.com>
2019-04-25 13:58:10 +0300

    edited home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,8 +12,11 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $json = file_get_contents(__DIR__ .'/../../students.json');
+        $data = json_decode($json, true);
+
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'projectsData' => $data
         ]);
     }
 }
