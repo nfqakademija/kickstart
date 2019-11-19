b17c023d1761e478d8f079061589c84fa8d97e91
am <miklovisarvydas@gmail.com>
2019-11-19 22:36:46 +0200

    homework task 8

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,8 +13,28 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $studentsData = file_get_contents('students.json');
         return $this->render('home/index.html.twig', [
-            'someVariable' => 'NFQ Akademija',
+            'students' => $this->groupByStudents(json_decode($studentsData, true)),
+            'projects' => $this->groupByTeams(json_decode($studentsData, true)),
         ]);
+    }
+    private function groupByTeams(array $projects)
+    {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+            $result[] = ['name' => $projectName, 'team' => $project['name']];
+        }
+        return $result;
+    }
+    private function groupByStudents(array $projects)
+    {
+        $result = [];
+        foreach ($projects as $projectName => $project) {
+            foreach ($project['students'] as $student) {
+                $result[] = ['student' => $student, 'project' => $projectName, 'mentors' => $project['mentors']];
+            }
+        }
+        return $result;
     }
 }
---
+++ src/Controller/StudentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function student(Request $request)
+    {
+        $name = $request->query->get('name');
+        $project = $request->query->get('project');
+        return $this->render('home/student.html.twig', [
+            'name' => $name,
+            'project' => $project,
+        ]);
+    }
+}
