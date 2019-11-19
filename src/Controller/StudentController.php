55ad084443a34234050b2fd389b8a184e23003a0
Montekarl <karolis.petrikas@mail.com>
2019-11-19 20:38:29 +0000

    Kernel interface

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StudentController extends AbstractController
@@ -10,10 +12,12 @@ class StudentController extends AbstractController
     /**
      * @Route("/student", name="student")
      */
-    public function index()
+    public function index(KernelInterface $request)
     {
+
+        $a = $request->getProjectDir();
         return $this->render('student/index.html.twig', [
-            'controller_name' => 'StudentController',
+            'controller_name' => $a,
         ]);
     }
 }
