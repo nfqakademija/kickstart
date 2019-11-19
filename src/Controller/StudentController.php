0b4a44179a58769d6ac132b269f7a7298a7379a1
am <miklovisarvydas@gmail.com>
2019-11-19 22:44:17 +0200

    small fix

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -17,7 +17,7 @@ class StudentController extends AbstractController
     {
         $name = $request->query->get('name');
         $project = $request->query->get('project');
-        return $this->render('home/student.html.twig', [
+        return $this->render('student/index.html.twig', [
             'name' => $name,
             'project' => $project,
         ]);
