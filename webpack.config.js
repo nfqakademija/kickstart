7033622d703ea4482413f9e125727409725aeba9
marchiuzz <marchiuzz@marchiuzzs-MacBook-Pro.local>
2019-12-12 21:57:55 +0200

    fixedscripts!

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ Encore
         './assets/js/app.js',
         './assets/js/react.jsx'
     ])
-    .addEntry('validation',
+    .addEntry(
+        'validation',
         './assets/js/validation.js'
     )
     //.addEntry('page1', './assets/js/page1.js')
